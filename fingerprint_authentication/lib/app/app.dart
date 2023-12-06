f8575237259fc40c74d5038f981c0f63ea42cd5b
Amankhan004 <ak1389152@gmail.com>
2023-12-06 18:08:01 +0500

    FingerPrint_Authentication

---
+++ fingerprint_authentication/lib/app/app.dart
@@ -0,0 +1,18 @@
+
+import 'package:fingerprint_authentication/UI/homeview/home_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+@StackedApp(routes: [MaterialRoute(page: HomeView, initial: true)],
+dependencies: [
+
+Singleton(classType: NavigationService)
+
+])
+
+
+
+class App{
+
+
+}
